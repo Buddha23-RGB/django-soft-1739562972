d9c132029e3108bb6dcf181f926e1d190c41981e
Buddha23-RGB <joechmoe555@hotmail.com>
2025-05-31 10:20:34 -0600

    may_31

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+set -o nounset
+set -o pipefail
 
 python -m pip install --upgrade pip
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations
+# It's generally recommended to run `makemigrations` during development
+# and commit the migration files to your repository.
+# The build script should then only apply migrations.
+# python manage.py makemigrations 
 python manage.py migrate
 
 #__API_GENERATOR__
